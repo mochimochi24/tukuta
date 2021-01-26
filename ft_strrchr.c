713d41656dfc9208b183a60e826df2315261c554
Yayoi Ito <yayito@c1r1s8.42tokyo.jp>
2021-01-26 21:41:51 +0900

    rchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,45 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yayito <yayito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/26 18:17:23 by yayito            #+#    #+#             */
-/*   Updated: 2021/01/26 21:34:54 by yayito           ###   ########.fr       */
+/*   Created: 2021/01/26 21:33:55 by yayito            #+#    #+#             */
+/*   Updated: 2021/01/26 21:41:36 by yayito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	int			i;
-	char		*ret;
-	char		cc;
+	int		i;
+	char	*ret;
+	char	cc;
 
 	cc = (char)c;
 	i = 0;
 	while (s[i])
 	{
 		if (s[i] == cc)
-		{
 			ret = (char*)&s[i];
-			return (ret);
-		}
 		i++;
 	}
+	if (ret)
+		return (ret);
 	return (NULL);
 }
 
 /*
 **int		main(void)
 **{
-**	char	c = '3';
-**	char	s[10] = "123abcde";
-**	printf("%s\n", ft_strchr(s, c));
-**	printf("%s", strchr(s, c));
+**	char	c = 'a';
+**	char	s[10] = "123abzab";
+**	printf("%s\n", ft_strrchr(s, c));
+**	printf("%s", strrchr(s, c));
 **	return (0);
 **}
 */
