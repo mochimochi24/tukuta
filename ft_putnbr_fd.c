3b94cc3ee1589572b72a4b0f53a0893f3ac755dc
Yayoi Ito <yayito@c1r1s8.42tokyo.jp>
2021-02-01 14:01:13 +0900

    nbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,18 +6,12 @@
 /*   By: yayito <yayito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 09:55:05 by yayito            #+#    #+#             */
-/*   Updated: 2021/02/01 13:33:34 by yayito           ###   ########.fr       */
+/*   Updated: 2021/02/01 14:00:59 by yayito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
-
-//void	ft_putchar_fd(char c, int fd)
-//{
-//	write(fd, &c, 1);
-//		return ;
-//}
 
 void	ft_putnbr_fd(int n, int fd)
 {
@@ -28,6 +22,7 @@ void	ft_putnbr_fd(int n, int fd)
 	if (n == -2147483648)
 	{
 		ft_putstr_fd("-2147483648", fd);
+		return ;
 	}
 	if (n < 0)
 	{
@@ -42,26 +37,14 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	wri[i] = n;
 	while (i >= 0)
-	{
-		ft_putchar_fd(wri[i], fd);
-		i--;
-	}
+		ft_putchar_fd((wri[i--] + '0'), fd);
 	return ;
 }
 
 /*
 int		main(void)
 {
-	int		n = 123455;
-	int		i = 0;
-	int		wri[12];
-	while (n >= 10)
-	{
-		wri[i] = (n % 10);
-		n = (n / 10);
-		i++;
-	}
-	printf("%d, %d, %d, %d, %d, %d", wri[0], wri[1], wri[2], wri[3], wri[4], wri[5]);
+	ft_putnbr_fd(-483647, 1);
 	return (0);
 }
 */
