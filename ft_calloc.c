0c9573cf772951ea7c9c6c9effe81ea7bcbdcb22
Yayoi Ito <yayito@c1r1s8.42tokyo.jp>
2021-02-01 13:08:42 +0900

    f

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: yayito <yayito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 06:00:20 by yayito            #+#    #+#             */
-/*   Updated: 2021/02/01 11:56:47 by yayito           ###   ########.fr       */
+/*   Updated: 2021/02/01 13:08:30 by yayito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,15 @@
 void	*ft_calloc(size_t n, size_t size)
 {
 	void	*po;
-	size_t	i;
 
 	if (n == 0 || !n)
 		n = 1;
 	if (size == 0 || !size)
 		size = 1;
-	i = (n * size);
 	po = (void*)malloc(n * size);
 	if (!po)
 		return (NULL);
-	n *= size;
-	while (i > 0)
-	{
-		po = 0;
-		po++;
-		i--;
-	}
+	ft_bzero(po, n * size);
 	return (po);
 }
 
