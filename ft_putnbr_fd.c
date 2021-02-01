bfa1a8f43aae8ed0d4dfb78d0963522bc6475685
Yayoi Ito <yayito@c1r1s8.42tokyo.jp>
2021-02-01 13:33:46 +0900

    fix

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,23 @@
 /*   By: yayito <yayito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 09:55:05 by yayito            #+#    #+#             */
-/*   Updated: 2021/01/31 11:17:27 by yayito           ###   ########.fr       */
+/*   Updated: 2021/02/01 13:33:34 by yayito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+
+//void	ft_putchar_fd(char c, int fd)
+//{
+//	write(fd, &c, 1);
+//		return ;
+//}
 
 void	ft_putnbr_fd(int n, int fd)
 {
 	int		i;
-	char	wri[12];
+	int		wri[12];
 
 	i = 0;
 	if (n == -2147483648)
@@ -24,13 +31,13 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (n < 0)
 	{
-		ft_putchar_fd("-", fd);
+		ft_putchar_fd(45, fd);
 		n *= -1;
 	}
-	while (n < 10)
+	while (n >= 10)
 	{
 		wri[i] = (n % 10);
-		n /= 10;
+		n = (n / 10);
 		i++;
 	}
 	wri[i] = n;
@@ -42,8 +49,19 @@ void	ft_putnbr_fd(int n, int fd)
 	return ;
 }
 
+/*
 int		main(void)
 {
-	ft_putnbr_fd(123455, 1);
+	int		n = 123455;
+	int		i = 0;
+	int		wri[12];
+	while (n >= 10)
+	{
+		wri[i] = (n % 10);
+		n = (n / 10);
+		i++;
+	}
+	printf("%d, %d, %d, %d, %d, %d", wri[0], wri[1], wri[2], wri[3], wri[4], wri[5]);
 	return (0);
 }
+*/
