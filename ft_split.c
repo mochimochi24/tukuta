a7227b61ca2b19c0a33cff5de7fd5a0c051772ae
Yayoi Ito <yayito@c1r1s8.42tokyo.jp>
2021-02-05 22:22:37 +0900

    splitok

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: yayito <yayito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 21:57:10 by yayito            #+#    #+#             */
-/*   Updated: 2021/02/05 20:18:30 by yayito           ###   ########.fr       */
+/*   Updated: 2021/02/05 22:21:53 by yayito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,9 @@ static char	**makepoipoi(char const *s, char c)
 		i++;
 	}
 	poipoi = (char**)malloc(sizeof(char*) * (yousosu + 1));
-
-//	DEBUG
-//	printf("size...%lu\n", (sizeof(char*) * (yousosu + 1));
-
 	if (!poipoi)
 		return (NULL);
 	poipoi[yousosu] = NULL;
-
-//	DEBUG
-//	printf("yousosu...%d\n", yousosu);
-
 	return (poipoi);
 }
 
@@ -90,10 +82,12 @@ static char **fillret(char const *s, char c, char **ret)
 	nn = 0;
 	j = 0;
 	i = 0;
-	while (ret[i] && s[j])
+	while (s[j])
 	{
 		while (s[j] == c)
 			j++;
+		if (!s[j])
+			break ;
 		leng = partlen(s, c, j);
 		ret[i] = (char*)malloc(leng + 1);
 		if (!ret[i])
@@ -102,25 +96,6 @@ static char **fillret(char const *s, char c, char **ret)
 			ret[i][nn++] = s[j++];
 		ret[i][nn] = '\0';
 		nn = 0;
-/*
-//		DEBUG
-		printf("i = %d\n",i);
-		printf("partlen...%d\n",leng);
-		printf("ret[0]...%s\n",ret[0]);
-		if (i >= 1)
-			printf("ret[1]...%s\n",ret[1]);
-		if (i >= 2)
-			printf("ret[2]...%s\n",ret[2]);
-		if (i >= 3)
-			printf("ret[3]...%s\n",ret[3]);
-		if (i >= 4)
-			printf("ret[4]...%s\n",ret[4]);
-		if (i >= 5)
-			printf("ret[5]...%s\n",ret[5]);
-		if (i >= 6)
-			printf("ret[6]...%s\n",ret[6]);
-		printf("\n");
-*/
 		i++;
 	}
 	return (ret);
@@ -139,28 +114,28 @@ char	**ft_split(char const *s, char c)
 }
 
 /*
-int		main(void)
-{
-//	char	str[50] = "777fourth!!";
-	char	str[50] = "      split       this for   me  !       ";
-//	char	str[50] = "olol                     ";
-	char	*ptr;
-	char	**ret;
-	int		i = 0;
-
-	ptr = &str[0];
-	ret = ft_split(ptr, ' ');
-	printf("result:\n");
-//	printf("\nresult...\n%s\n", re[0]);
-//	printf("%s\n", re[1]);
-//	printf("%s\n", re[2]);
-//	printf("%s", re[3]);
-	while (ret[i] != 0)
-	{
-		printf("ret[%d]...%s\n",i , ret[i]);
-		i++;
-	}
-	printf("ret[%d]...%s = %p",i , ret[i], ret[i]);
-	return (0);
-}
+**int		main(void)
+**{
+**	char	str[50] = "";
+**	char	str[50] = "      split       this for   me  !       ";
+**	char	str[50] = "olol                     ";
+**	char	*ptr;
+**	char	**ret;
+**	int		i = 0;
+**
+**	ptr = &str[0];
+**	ret = ft_split(ptr, ' ');
+**	printf("result:\n");
+**	printf("\nresult...\n%s\n", re[0]);
+**	printf("%s\n", re[1]);
+**	printf("%s\n", re[2]);
+**	printf("%s", re[3]);
+**	while (ret[i] != 0)
+**	{
+**		printf("ret[%d]...%s\n",i , ret[i]);
+**		i++;
+**	}
+**	printf("ret[%d]...%s = %p",i , ret[i], ret[i]);
+**	return (0);
+**}
 */
